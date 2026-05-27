cb3aa25c230654994fd817a473d3c025c8064ec0
Johana Alejandra Gonzalez Macias <l211050235@pabellon.tecnm.mx>
2026-05-27 14:01:56 -0600

    Se agregaron los nuevos negocios que participaron en ruta de la garnacha, falta cambiar informacion, como direccio y numero de telefono

--- js/garnacha.js
+++ js/garnacha.js
@@ -1,72 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-    $('.GyMI').click(function() {
-        $( '.res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res1' ).show( "slow" );
+    
+
+    var EDICION2025 = $('.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14');
+    var EDICION2026 = $('.res15, .res16, .res17, .res18, .res19, .res20, .res21, .res22, .res23');
+
+    // Evento para la edición 2025
+    $('.EDICION2025').click(function() {
+        $('.none').fadeOut();// Ocultamos el mensaje de "vacío"
+        EDICION2026.fadeOut(); // Ocultamos lo de 2026 por si estaba abierto
+        EDICION2025.show("slow"); // Mostramos 2025
     });
 
-    $('.GT').click(function() {
-        $( '.res1, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res2' ).show( "slow" );
-    });
-
-    $('.LS').click(function() {
-        $( '.res1, .res2, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res3' ).show( "slow" );
-    });
-
-    $('.MC').click(function() {
-        $( '.res1, .res2, .res3, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res4' ).show( "slow" );
-    });
-
-    $('.GA').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res5' ).show( "slow" );
-    });
-
-    $('.AC').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res6' ).show( "slow" );
-    });
-
-    $('.CL').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res8, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res7' ).show( "slow" );
-    });
-
-    $('.CH').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res9, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res8' ).show( "slow" );
-    });
-
-    $('.GM').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res10, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res9' ).show( "slow" );
-    });
-
-    $('.TY').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res11, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res10' ).show( "slow" );
-    });
-    $('.LC').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res12, .res13, .res14, .none').fadeOut();
-        $( '.res11' ).show( "slow" );
-    });
-    $('.RP').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res13, .res14, .none').fadeOut();
-        $( '.res12' ).show( "slow" );
-    });
-    $('.CN').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res14, .none').fadeOut();
-        $( '.res13' ).show( "slow" );
-    });
-    $('.RC').click(function() {
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .none').fadeOut();
-        $( '.res14' ).show( "slow" );
-    });
-    $('.todo').click(function() {
-        $( '.none' ).fadeOut(); // Ocultamos el mensaje de "vacío" si existe
-        $( '.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14' ).show( "slow" );
+    // Evento para la edición 2026
+    $('.EDICION2026').click(function() {
+        $('.none').fadeOut();// Ocultamos el mensaje de "vacío"
+        EDICION2025.fadeOut(); // Ocultamos lo de 2025
+        EDICION2026.show("slow"); // Mostramos 2026
     });
 
 
