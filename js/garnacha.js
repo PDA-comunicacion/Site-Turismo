0aff63246704824e5b90fed5b8a2b9d7765deb80
Johana Alejandra Gonzalez Macias <l211050235@pabellon.tecnm.mx>
2026-05-28 23:13:02 -0600

    Se agregaron nuevas pestañas para mejor rendimiento y asi mismo para mejor funcionamiento del carrusel

--- js/garnacha.js
+++ js/garnacha.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var EDICION2025 = $('.res1, .res2, .res3, .res4, .res5, .res6, .res7, .res8, .res9, .res10, .res11, .res12, .res13, .res14');
     var EDICION2026 = $('.res15, .res16, .res17, .res18, .res19, .res20, .res21, .res22, .res23');
 
+
     // Evento para la edición 2025
     $('.EDICION2025').click(function() {
         $('.none').fadeOut();// Ocultamos el mensaje de "vacío"
